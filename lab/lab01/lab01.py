6cb6deb8a419df457d28b8bdd6988dec00a976bc
yansong <liys0304@163.com>
2024-09-09 17:31:02 +0800

    lab01

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,68 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    ret = 1
+    while k > 0:
+        ret *= n
+        k -= 1
+        n -= 1
+    return ret
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    ret = 0
+    curr, left = 0, y
+    while left >= 10:
+        curr = left % 10
+        left = left // 10
+        ret = ret + curr
+    return ret + left
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    left = n
+    while left >= 88:
+        curr = left % 100
+        if curr == 88:
+            return True
+        left //= 10
+    return False
+
